60df6ef80ea0e182aa2e93e9fd1c2c481a913ee3
Shumaila550 <codecanvas78@gmail.com>
2026-02-13 15:25:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Scroll animation for cards
-const cards = document.querySelectorAll(".card");
+const posts=JSON.parse(localStorage.getItem("posts"));
 
-window.addEventListener("scroll",()=>{
-  const trigger = window.innerHeight * 0.85;
-
-  cards.forEach(card=>{
-    const top = card.getBoundingClientRect().top;
-    if(top < trigger){
-      card.classList.add("show");
-    }
-  });
+let html="";
+posts.forEach((p,i)=>{
+html+=`
+<div class="card">
+<h3>${p.title}</h3>
+<p>${p.summary}</p>
+<a href="post.html?id=${i}">Read more</a>
+</div>`;
 });
+
+document.getElementById("posts").innerHTML=html;
+
+
+// DARK MODE
+function toggleTheme(){
+document.body.classList.toggle("dark");
+localStorage.setItem("theme",
+document.body.classList.contains("dark")?"dark":"light");
+}
+
+if(localStorage.getItem("theme")==="dark"){
+document.body.classList.add("dark");
+}
