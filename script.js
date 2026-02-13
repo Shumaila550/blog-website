79f8cb1b1ec8de3e8759874915ab4d258889cd71
Shumaila550 <codecanvas78@gmail.com>
2026-02-13 14:55:00 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch('posts.json')
+.then(res => res.json())
+.then(posts => {
+
+    let html = "";
+
+    posts.forEach((post, index) => {
+        html += `
+        <div class="post">
+            <h2>${post.title}</h2>
+            <p>${post.summary}</p>
+            <a href="post.html?id=${index}">Read Article</a>
+        </div>
+        `;
+    });
+
+    document.getElementById("posts").innerHTML = html;
+});
